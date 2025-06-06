af88989e3a24811578aad8a9a3a2a3988ffbf659
whacked <whacked@users.noreply.github.com>
2025-06-06 16:14:41 +0800

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,43 @@
+{ pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [
+        (import "${fetchTree gomod2nix.locked}/overlay.nix")
+      ];
+    }
+  )
+, mkGoEnv ? pkgs.mkGoEnv
+, gomod2nix ? pkgs.gomod2nix
+}:
+
+let
+  goEnv = mkGoEnv { pwd = ./.; };
+  go-jsonschema = pkgs.stdenv.mkDerivation {
+    name = "go-jsonschema";
+    src = pkgs.fetchurl {
+      url = "https://github.com/omissis/go-jsonschema/releases/download/v0.15.0/go-jsonschema_Linux_x86_64.tar.gz";
+      sha256 = "diR8EUGrEcVyhW5kAyDyHluoWRnj3lUlNL2BbhUjFS4=";
+    };
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out/bin
+      tar -xzf $src -C $out/bin
+    '';
+    buildInputs = [ pkgs.unzip ];
+  };
+in
+pkgs.mkShell {
+  packages = [
+    goEnv
+    gomod2nix
+    pkgs.check-jsonschema
+    go-jsonschema
+  ];
+
+  shellHook = ''
+    export PATH=$PWD:$PATH
+  '';
+}
